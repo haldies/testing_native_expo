95ab058d6b07693fb360537bdc89cf9e65bcc20e
Haldies Gerhardien Pasya <haldiespasya@gmail.com>
2026-04-09 14:57:44 +0700

    feat: add MemoryModule for native Siri shortcut and memory data management

--- ios/MyApp/MemoryModule.swift
+++ ios/MyApp/MemoryModule.swift
@@ -20,7 +20,7 @@ class MemoryModule: NSObject {
   @objc(refreshShortcuts:reject:)
   func refreshShortcuts(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
     if #available(iOS 16.0, *) {
-      AppIntents.AppShortcutsCenter.shared.updateAppShortcutParameters()
+      MyAppShortcuts.updateAppShortcutParameters()
       resolve(true)
     } else {
       resolve(false)
